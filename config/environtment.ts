762e85788ac3c115cf34296b57c5c13417bb5f08
satriaherman <45140436+satriaherman1@users.noreply.github.com>
2022-03-23 19:53:34 +0700

    Update baseUrl

--- config/environtment.ts
+++ config/environtment.ts
@@ -1,3 +1,3 @@
-const baseUrl = "https://main-api-ipnu.herokuapp.com/v1";
+const baseUrl = "https://docs-management-api.satriaherman.com/v1";
 const DRIVE_URL = "https://drive.google.com/uc?id=";
 export { baseUrl, DRIVE_URL };
